bebdc3e2a11b3e281f6b236f37be250e542f577a
Victor Torres Mori <vtorremo@emeal.nttdata.com>
2023-08-08 17:07:02 -0500

    Checar si existi archivo

--- bdArchivoChequeo.js
+++ bdArchivoChequeo.js
@@ -1,7 +1,9 @@
 import fs from 'fs';
 import { exit } from "process"
 
-if (!fs.existsSync("bd.json")) {
-    console.log("El archivo existe!");
+export default function dbFileCheck() {
+  if (!fs.existsSync("bd.json")) {
+    console.log("La Base de Datos está vacía. ¡Crea algún dato!");
     exit(1);
+  }
 }
